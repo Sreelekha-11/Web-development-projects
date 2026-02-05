03669332fb8f862402fff0fbf3f08c42c79e06cd
Sreelekha <thippireddysreelekha@gmail.com>
2026-02-05 08:04:20 -0800

    Add files via upload

---
+++ Project1-Form validation/login.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const loginPassword = document.getElementById("loginpassword");
+    const loginError = document.getElementById("loginerror");
+
+    loginPassword.addEventListener("input", function () {
+
+        if (loginPassword.value.length < 6) {
+            loginError.innerText = "Password must contain 6 letters";
+            loginError.style.color = "red";
+            return;
+        }
+
+
+        loginError.innerText = "";
+    });
+
+
+});
